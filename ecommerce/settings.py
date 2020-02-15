229b2597c2d45780c1b3a70edb489d81713712ee
Claudia De Beer <claudiadiebeer@gmail.com>
2020-02-15 18:55:01 +0000

    fix allowed_hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'https://ci-ecom-site.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'ci-ecom-site.herokuapp.com/']
 
 
 # Application definition
